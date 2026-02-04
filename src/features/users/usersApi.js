5474cc9ff25e11a31c57d38f6af9ce421e3d96b9
M Ahmad Amin <ahmadamin1005@gmail.com>
2026-02-05 00:50:47 +0500

    Name fix: File added

---
+++ src/features/users/usersApi.js
@@ -0,0 +1,84 @@
+import apiSlice from '../../api/apiSlice';
+
+const usersApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUser: builder.query({
+      query: (id) => ({
+        url: `/user/${id}`,
+        method: 'get',
+      }),
+      providesTags: (result, error, id) => [{ type: 'Users', id }],
+    }),
+
+    getUsers: builder.query({
+      query: ({
+        page = 1,
+        limit = 10,
+        search = '',
+        role = '',
+        status = '',
+      }) => {
+        // Build query params
+        const params = new URLSearchParams({
+          page,
+          limit,
+        });
+
+        if (search) params.append('search', search);
+        if (role && role !== 'all') params.append('role', role);
+        if (status && status !== 'all') params.append('status', status);
+
+        return {
+          url: `/user?${params.toString()}`,
+          method: 'get',
+        };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.data.users.map(({ _id }) => ({
+                type: 'Users',
+                id: _id,
+              })),
+              { type: 'Users', id: 'LIST' },
+            ]
+          : [{ type: 'Users', id: 'LIST' }],
+    }),
+
+    inviteUser: builder.mutation({
+      query: (userData) => ({
+        url: `/user`,
+        method: 'post',
+        data: userData,
+      }),
+      invalidatesTags: [{ type: 'Users', id: 'LIST' }],
+    }),
+
+    updateUser: builder.mutation({
+      query: ({ id, ...patchData }) => ({
+        url: `/user/${id}`,
+        method: 'patch',
+        data: patchData,
+      }),
+      invalidatesTags: (result, error, { id }) => [{ type: 'Users', id }],
+    }),
+
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/user/${id}`,
+        method: 'delete',
+      }),
+      invalidatesTags: [{ type: 'Users', id: 'LIST' }],
+    }),
+  }),
+});
+
+export const {
+  useGetUsersQuery,
+  useGetUserQuery,
+  useInviteUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = usersApi;
+
+export default usersApi;
